9f61f1843fc69c215087e9a2f2f07cfb0d3912f1
Scott Brownlie <scottbrownlie1988@gmail.com>
2020-06-01 08:13:09 -0300

    finish lab 1

--- mlp/data_providers.py
+++ mlp/data_providers.py
@@ -133,11 +133,11 @@ class MNISTDataProvider(DataProvider):
         super(MNISTDataProvider, self).__init__(
             inputs, targets, batch_size, max_num_batches, shuffle_order, rng)
 
-    # def next(self):
-    #    """Returns next data batch or raises `StopIteration` if at end."""
-    #    inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
-    #    return inputs_batch, self.to_one_of_k(targets_batch)
-    #
+    def next(self):
+        """Returns next data batch or raises `StopIteration` if at end."""
+        inputs_batch, targets_batch = super(MNISTDataProvider, self).next()
+        return inputs_batch, self.to_one_of_k(targets_batch)
+
     def __next__(self):
         return self.next()
 
@@ -156,7 +156,10 @@ class MNISTDataProvider(DataProvider):
             to zero except for the column corresponding to the correct class
             which is equal to one.
         """
-        raise NotImplementedError()
+        N, K = len(int_targets), int_targets.max() + 1
+        one_hot = np.zeros((N, K))
+        one_hot[range(N), int_targets] = 1
+        return one_hot
 
 
 class MetOfficeDataProvider(DataProvider):
@@ -188,19 +191,24 @@ class MetOfficeDataProvider(DataProvider):
             'Data file does not exist at expected path: ' + data_path
         )
         # load raw data from text file
-        # ...
+        data = np.loadtxt(data_path, skiprows=3)[:, 2:]
         # filter out all missing datapoints and flatten to a vector
-        # ...
+        x = data[data >= 0]
         # normalise data to zero mean, unit standard deviation
-        # ...
+        x = (x - x.mean()) / x.std()
         # convert from flat sequence to windowed data
-        # ...
+        n_sequences = len(x) - window_size + 1
+        i = np.tile(np.arange(window_size), (n_sequences, 1)).astype(int)
+        offsets = np.tile(np.ones(window_size), (n_sequences, 1)).astype(int) * np.arange(n_sequences)[:, None]
+        i = i + offsets
+        sequences = x[i]
         # inputs are first (window_size - 1) entries in windows
-        # inputs = ...
+        inputs = sequences[:, :-1]
         # targets are last entry in windows
-        # targets = ...
+        targets = sequences[:, -1]
         # initialise base class with inputs and targets arrays
-        # super(MetOfficeDataProvider, self).__init__(
-        #     inputs, targets, batch_size, max_num_batches, shuffle_order, rng)
+        super(MetOfficeDataProvider, self).__init__(
+            inputs, targets, batch_size, max_num_batches, shuffle_order, rng)
+
     def __next__(self):
             return self.next()
